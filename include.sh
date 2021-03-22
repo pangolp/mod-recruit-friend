8437e7cf2fca96df7b5aa34dabcee86900a78669
Pagani Walter <paganiwalter@gmail.com>
2021-03-22 16:20:25 -0300

    Configuring the assembler

--- include.sh
+++ include.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 ## GETS THE CURRENT MODULE ROOT DIRECTORY
-MOD_SKELETON_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )/" && pwd )"
+MOD_RECRUIT_FRIEND_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )/" && pwd )"
 
-source $MOD_SKELETON_ROOT"/conf/conf.sh.dist"
+source $MOD_RECRUIT_FRIEND_ROOT"/conf/conf.sh.dist"
 
-if [ -f $MOD_SKELETON_ROOT"/conf/conf.sh" ]; then
-    source $MOD_SKELETON_ROOT"/conf/conf.sh"
+if [ -f $MOD_RECRUIT_FRIEND_ROOT"/conf/conf.sh" ]; then
+    source $MOD_RECRUIT_FRIEND_ROOT"/conf/conf.sh"
 fi
