b48e72f539e5a842ba9a24ac97c583054e49c21a
Walter Pagani <paganiwalter@gmail.com>
2023-07-28 00:14:42 -0300

    feat. New features (#7)

--- data/sql/db-world/updates/recruit_friend_acore_string_add_cooldown.sql
+++ data/sql/db-world/updates/recruit_friend_acore_string_add_cooldown.sql
@@ -1,4 +1,5 @@
 SET @ENTRY:=35457;
-DELETE FROM `acore_string` WHERE `entry`=@ENTRY+0;
+DELETE FROM `acore_string` WHERE `entry` IN (@ENTRY+0, @ENTRY+1);
 INSERT INTO `acore_string` (`entry`, `content_default`, `locale_koKR`, `locale_frFR`, `locale_deDE`, `locale_zhCN`, `locale_zhTW`, `locale_esES`, `locale_esMX`, `locale_ruRU`) VALUES
-(@ENTRY+0, '[Recruit Friend] You have to wait the cooldown to recruit a friend', '', '', '', '', '', '[Reclutar Amigo] Debes esperar el cooldown para reclutar un amigo', '[Reclutar Amigo] Debes esperar el cooldown para reclutar un amigo', '');
+(@ENTRY+0, '[Recruit Friend] You must wait for %u sec. to use this command again.', '', '', '', '', '', '[Reclutar Amigo] Debes esperar %u seg para utilizar este comando nuevamente.', '[Reclutar Amigo] Debes esperar %u seg para utilizar este comando nuevamente.', ''),
+(@ENTRY+1, '[Recruit Friend] You haven\'t recruited anyone. Use the .recruit add command to do this.', '', '', '', '', '', '[Recruit Friend] No has reclutado a nadie. Utiliza el comando .recruit add para hacerlo.', '[Recruit Friend] No has reclutado a nadie. Utiliza el comando .recruit add para hacerlo.', '');
